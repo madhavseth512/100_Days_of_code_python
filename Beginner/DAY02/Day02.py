efa7a7ac10bc8521bb2fb66a3973e4462552d74a
madhavseth512 <sethmadhav125@gmail.com>
2024-06-14 23:31:21 +0530

    Added code for day02

--- Beginner/DAY02/Day02.py
+++ Beginner/DAY02/Day02.py
@@ -43,4 +43,35 @@ print(70+float("100.5"))
 # Here converting the integer data type to string. Output will be 70100
 print(str(70)+str(100))
 
+# Mathematical Operators
+print(23+43)
+print(93-25)
+print(3*2)
+# Here in python programming language the division gives the result in float data type
+print(6/3)
+# Exponential mathematical operator is ** (2^3 is written as) -
+print(2**3)
+
+# round((What_you_want_to_round_off),precision) function rounds off our numerical value
+print(round(6.7))  # This rounds off all the decimal values
+print(round(5.6666, 2))  # Rounds off the number to 2 decimal places i.e. result - 5.67
+
+# FLOOR DIVISION
+# The floor division discards the decimal part and gives only the integer and on running the type check
+# on the output we get int data type
+print(10 // 3)  # Output - 3
+print(15 // 9)  # Output - 1
+
+# F-Strings
+# These are called formatted strings.
+# It is to embed all the data types into the strings without converting them into strings.
+height = 1.8
+isWinning = True
+score = 41
+# Notice that in the syntax we have used the letter f in front of double/single quotes
+# Used curly braces for variable names which are of other data types
+print(f"Your score is {score}. Your height is {height}. The prob that you are winning is {isWinning}")
+
+# Unlike C++ if both the numerator and denominator are of integer data type then the answer would be in float data type.
+print(5/2)
 
