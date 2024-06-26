ccba38b0da1aa8e7f20cb86ff72b4db3e0f5e6c8
madhavseth512 <sethmadhav125@gmail.com>
2024-06-26 23:52:14 +0530

    Added python file for Day06

---
+++ Beginner/DAY06/Day06.py
@@ -0,0 +1,21 @@
+# Functions
+# There are many in built functions in python that we are using till now like len(), round(), int() etc
+
+# We can create our own custom functions:
+def my_function():
+    print("hello")
+    print("cuties")
+
+
+# Calling the functions to execute the lines of code in it
+my_function()
+
+# Indentation is very important in the function also. Same indentation means it belongs to the same block of code
+
+# WHILE LOOP SYNTAX
+number = 6
+while number > 0:
+    print("Testing while loop")
+    number -= 1
+
+print("Now we are out of while loop")
